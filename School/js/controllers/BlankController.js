0eaf2833933c398f8808930dd8facde9725b6e42
Fincountant <33073614+Fincountant@users.noreply.github.com>
2017-11-20 13:56:14 +0500

    db update

--- School/js/controllers/BlankController.js
+++ School/js/controllers/BlankController.js
@@ -1,30 +1,30 @@
 /* Setup blank page controller */
-angular.module('MetronicApp').controller('BlankController', ['$rootScope', '$scope', 'settings','$http',
-    function ($rootScope, $scope, settings,$http) {
-    $scope.$on('$viewContentLoaded', function() {   
-        // initialize core components
-        App.initAjax();
+angular.module('MetronicApp').controller('BlankController', ['$rootScope', '$scope', 'settings', '$http',
+    function ($rootScope, $scope, settings, $http) {
+        $scope.$on('$viewContentLoaded', function () {
+            // initialize core components
+            App.initAjax();
 
-        // set default layout mode
-        $rootScope.settings.layout.pageContentWhite = true;
-        $rootScope.settings.layout.pageBodySolid = false;
-        $rootScope.settings.layout.pageSidebarClosed = false;
-    });
+            // set default layout mode
+            $rootScope.settings.layout.pageContentWhite = true;
+            $rootScope.settings.layout.pageBodySolid = false;
+            $rootScope.settings.layout.pageSidebarClosed = false;
+        });
 
 
-
+        $scope.loginPage = function()
+        {
             $scope.transact = "iqbal";
 
-            $http.post('/api/Values/AddPayment', $scope.transact)
+            $http.post('/api/Login/addLogin', $scope.transact)
                 .then(function (response) {
                     $scope.transact = {};
-                    alert(response.data);
 
                 }).catch(function (response) {
                     console.log(response.status);
                     alert("Something is wrong");
                 });
-        
 
+        }
 
-}]);
+    }]);
